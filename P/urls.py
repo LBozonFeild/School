f3f7eaac6e69b89851d41386e5857b00460ef4d1
Mahiyat Alam <mahiyatalam150@gmail.com>
2026-06-11 13:48:02 +0600

    add postgres support

--- P/urls.py
+++ P/urls.py
@@ -1,26 +1,17 @@
 from django.urls import path
 from a import views
 from django.contrib import admin
-urlpatterns = [
-    # combined login/signup page
-    path("", views.auth_page, name="auth"),
-
-    # authentication actions
-    path("login/", views.login_view, name="login"),
-    path("register/", views.register_view, name="register"),
-
-    # main system
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("logout/", views.logout_view, name="logout"),
-    path("admin/", admin.site.urls),
-]
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns = [
+    path("", views.auth_page),
+    path("login/", views.login_view),
+    path("register/", views.register_view),
+    path("dashboard/", views.dashboard),
+    path("logout/", views.logout_view),
+    path("admin/", admin.site.urls),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
